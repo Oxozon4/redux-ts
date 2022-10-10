03bb78c5fb0d052bb2ecb71a3f1a7f43abcec8c4
Oxozon <oxozon.o4@gmail.com>
2022-10-10 22:42:11 +0200

    refactor reducer w/ TS

--- src/state/repositoriesReducer.ts
+++ src/state/repositoriesReducer.ts
@@ -1,22 +1,49 @@
 interface RepositoriesState {
-    loading: boolean;
-    error: string | null;
-    data: string[];
-};
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+enum ActionType {
+    SEARCH_REPOSITORIES = "search_repositories",
+    SEARCH_REPOSITORIES_SUCCESS = "search_repositories_success",
+    SEARCH_REPOSITORIES_ERROR = "search_repositories_error"
+}
+
+interface SearchRepositoriesAction {
+  type: ActionType.SEARCH_REPOSITORIES;
+}
+
+interface SearchRepositoriesSucessAction {
+  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
+  payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+  type: ActionType.SEARCH_REPOSITORIES_ERROR;
+  payload: string;
+}
+
+type Action =
+  | SearchRepositoriesAction
+  | SearchRepositoriesSucessAction
+  | SearchRepositoriesErrorAction;
 
 
-
-const reducer = (state: RepositoriesState, action: any) => {
-    switch (action.type) {
-        case 'search_repositories':
-            return { loading: true, error: null, data: [] };
-        case 'search_repositories_success':
-            return { loading: false, error: null, data: action.payload };
-        case 'search_repositories_error':
-            return { loading: false, error: action.payload, data: [] };
-        default:
-            return state;
-    }
+const reducer = (
+  state: RepositoriesState,
+  action: Action
+): RepositoriesState => {
+  switch (action.type) {
+    case ActionType.SEARCH_REPOSITORIES:
+      return { loading: true, error: null, data: [] };
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      return { loading: false, error: null, data: action.payload };
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return { loading: false, error: action.payload, data: [] };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
